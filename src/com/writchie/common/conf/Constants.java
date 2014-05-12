e9745744a6c9ac1b0014d770544c85f40c036f83
wuliqi <wRitchie@wRitchie.com>
2014-05-13 07:58:31 +0800

    QQ appId

--- src/com/writchie/common/conf/Constants.java
+++ src/com/writchie/common/conf/Constants.java
@@ -9,8 +9,8 @@ public class Constants {
 	public static final String SKEY="wRitchie";//md5加密的密码
 	public static final boolean DEBUG=true;//调试标记：true表示在调试状态,false表示发布状态，默认为false
 	
-	public static final String APP_ID="101046388";//android app QQ所申请的app_id
-	public static final String APP_KEY="0b95c65dd5f589c7c35762ecd51cb62c";//android app QQ所申请的app_key
+	public static final String APP_ID="1101366373";//android app QQ所申请的app_id
+	public static final String APP_KEY="Ye9j4ELCErn2V3uG";//android app QQ所申请的app_key
 	
 	public static final String BAIDU_ACCESS_KEY="geL0d8AwOyDphMSwdpHr00A6";//百度地图key
 	
